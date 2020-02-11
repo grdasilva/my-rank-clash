cb60f08f137ecfbea28b7dd0625931917593e7f0
G. Rodrigues da Silva <g.rodrigues.da.silva@avanade.com>
2020-02-11 11:16:08 -0300

    alterando ipconfig da chamada do proxy

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { Routes, RouterModule } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    loadChildren: () => import('./components/body/body.module').then(m => m.BodyModule)
+    loadChildren: () => import('./components/user-profile/user-profile.module').then(m => m.UserProfileModule)
   },
   {
     path: 'userProfile',
